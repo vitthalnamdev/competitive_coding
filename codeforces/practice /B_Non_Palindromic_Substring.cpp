05b6ecfcacd0c23eb8b23ba20f5ec1fbd9f4d53b
vitthal <vitthal7804@gmail.com>
2024-04-13 15:20:41 +0530

    ok

---
+++ codeforces/practice /B_Non_Palindromic_Substring.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll =long long int;
+using ull=unsigned long long;
+
+ void solve(){
+       int n,q;cin>>n>>q;
+       string s;cin>>s;
+       
+      while(q--)
+      {
+         
+      }
+return;
+}
+int main()
+{
+std::ios::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr);
+int t;
+cin>>t;
+// t=1;
+while(t--)
+solve();
+return 0;
+}
--- codeforces/practice /C_k_Tree.cpp
+++ codeforces/practice /C_k_Tree.cpp
@@ -2,19 +2,36 @@
 using namespace std;
 using ll =long long int;
 using ull=unsigned long long;
-
+#define mod 1000000007
 void solve(){
   ll n,k,d;cin>>n>>k>>d;
-  int mod=1e9+7;  
-  int dp[n+1];
-  return;
+  ll dp[n+1];ll smalldp[n+1];
+  for(int i=0;i<=n;i++)dp[i]=0,smalldp[i]=0;
+  smalldp[0]=1;
+  for(int i=0;i<=n;i++){
+    for(int j=1;j<=k;j++){
+      if(j<d && i+j<=n){
+         smalldp[j+i]+=smalldp[i];
+         smalldp[j+i]%=mod;
+      }
+      if(j>=d && i+j<=n){
+         dp[i+j]+=smalldp[i];
+         dp[i+j]%=mod;
+      }
+      if(i+j<=n){
+       dp[i+j]+=dp[i];
+       dp[i+j]%=mod;
+      }
+   }
+  }  
+  cout<<dp[n]<<endl;
 }
 int main()
 {
 std::ios::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr);
 int t;
-cin>>t;
-// t=1;
+ 
+  t=1;
 while(t--)
 solve();
 return 0;
