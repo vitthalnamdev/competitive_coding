5f5e5bdc2ae23020c3cd2ef651b3b70b3c314b78
vitthal <vitthal7804@gmail.com>
2024-04-15 16:27:12 +0530

    point update

--- codeforces/practice /segment trees/application.cpp
+++ codeforces/practice /segment trees/application.cpp
@@ -14,6 +14,16 @@ int construct(int start,int end,int ind){
     tree[ind]=max(left,right);
     return tree[ind];
 }
+int query_update(int start,int end,int ind,int ele,int index)
+{
+   if(start==end & start==index){arr[index]=ele; return tree[ind]=ele;}
+   else if(start==end){return tree[ind];}
+   if(end<index || start>index){return tree[ind];}
+   int mid=(start+end)/2;
+   int left=query_update(start,mid,2*ind+1,ele,index);
+   int right=query_update(mid+1,end,2*ind+2,ele,index); 
+   return tree[ind]=max(left,right);
+}
 int query(int start,int end,int ind,int l,int r)
 {
    if(end<l || start>r){return INT_MIN;}
@@ -27,12 +37,18 @@ int main(){
    int n;cin>>n;
   for(int i=0;i<n;i++)cin>>arr[i];
   construct(0,n-1,1);
-  //cout<<tree[1]<<endl;
    int q;cin>>q;
    while(q--)
    {
-    int l,r;cin>>l>>r;
-    l--;r--;
-    cout<<query(0,n-1,1,l,r);
+    int type;cin>>type;
+    if(type==1){
+        
+        int ind,ele;cin>>ind>>ele;ind--;
+        query_update(0,n-1,1,ele,ind);
+       // cout<<tree[4]<<endl;
+    }else{
+        int l,r;cin>>l>>r;l--;r--;
+        cout<<query(0,n-1,1,l,r)<<endl;
+    }
    }
 }
