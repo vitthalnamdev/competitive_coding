ba5708f3679abfb1e807364ac7b152b050f13a6f
vitthal <vitthal7804@gmail.com>
2024-05-06 14:26:29 +0530

    modular_inverse

---
+++ codeforces/practice /modular_inverse_function/app.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll =long long int;
+using ull=unsigned long long;
+int mod = 1e9+7;
+
+ll inv(int a) {
+  return a <= 1 ? a : mod - (long long)(mod/a) * inv(mod % a) % mod;
+}
+ void solve(){
+       
+
+
+
+
+
+return;
+}
+int main()
+{
+std::ios::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr);
+int t;
+cin>>t;
+// t=1;
+while(t--)
+solve();
+return 0;
+}
