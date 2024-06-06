f7f41f533ff37f1467088a83ce32d84b74ea5cba
vitthal namdev <vitthal7804@gmail.com>
2024-06-06 18:17:34 +0530

    number theory

---
+++ codeforces/practice /C_Road_Optimization.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+  #define ll long long 
+void solve(){
+int n,l,k;cin>>n>>l>>k;
+int corr[n+1],speed[n];
+for(int i=0;i<n;i++)cin>>corr[i];
+corr[n]=l;
+for(int i=0;i<n;i++)cin>>speed[i];
+int dp[n+1][k+1];                                                                                                                                                                                                                                                         
+
+}
+int main(){
+std::ios::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr);
+int t=1;
+while(t--){
+solve();
+}
+}
---
+++ codeforces/practice /D_GCD_sequence.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+  #define ll long long 
+ 
+void solve(){
+  int n;cin>>n;
+  int cnt1=0;
+  int arr[n];
+  for(int i=0;i<n;i++)cin>>arr[i];
+  
+}
+int main(){
+std::ios::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr);
+int t;cin>>t;
+while(t--){
+solve();
+}
+}
